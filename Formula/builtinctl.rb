78b9db047de5c2ca2f51436c4fac0005134d50c3
d1scolor <d1scolor@users.noreply.github.com>
2026-08-08 19:18:44 +1000

    Satisfy Homebrew formula audit

--- Formula/builtinctl.rb
+++ Formula/builtinctl.rb
@@ -2,13 +2,15 @@ class Builtinctl < Formula
   desc "Control and automate the Mac built-in display"
   homepage "https://github.com/d1scolor/builtinctl"
   url "https://github.com/d1scolor/builtinctl/releases/download/v0.1.0/builtinctl-0.1.0-macos-arm64.tar.gz"
-  version "0.1.0"
   sha256 "c72436fac62cada849cc2608c05df0dc1f786fb4de4b7d5d7dd5c8d15aa516e8"
   license "MIT"
 
   depends_on arch: :arm64
   depends_on :macos
-  depends_on macos: :ventura
+
+  on_macos do
+    depends_on macos: :ventura
+  end
 
   def install
     bin.install "builtinctl"
